0036746c9901e185d5e96487f5823750c0aa5e24
Zubaer <bijon7@yahoo.com>
2021-11-07 01:31:58 -0700

    add revised version of query for teacher

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,14 +1,14 @@
-pool.query(`SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = 'JUL02'
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
 ORDER BY teacher;
-
 `)
   .then(res => {
-    res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
     })
   });
