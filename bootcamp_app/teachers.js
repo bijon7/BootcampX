82e4a7346c3897dcf58a5982f22613a468df1f2e
Zubaer <bijon7@yahoo.com>
2021-11-06 23:41:32 -0700

    Add javascript queries to database in bootcampx project

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,14 @@
+pool.query(`SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+ORDER BY teacher;
+
+`)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    })
+  });
